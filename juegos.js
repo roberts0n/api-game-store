f9b3dac6e6a5342359fe36c4db6ec19771c5363c
Roberto <kaiser.dark4@outlook.cl>
2024-05-31 01:28:03 -0400

    steam imagenes

--- juegos.js
+++ juegos.js
@@ -15,7 +15,8 @@ const juegos = [
         "categoria" : "RPG",
         "plataformas" : "Steam",
         "precio" : 60000,
-        "stock" : "Solo clave digital"
+        "stock" : "Solo clave digital",
+        "imagen" : "https://image.api.playstation.com/vulcan/ap/rnd/202311/1309/746f848d0be7107e301df1142745b334b64fbc87c218e3b1.jpg"
         
     },
     {
@@ -24,7 +25,8 @@ const juegos = [
         "categoria" : "Accion",
         "plataformas" : "Steam",
         "precio" : 48000,
-        "stock" : "Solo clave digital"
+        "stock" : "Solo clave digital",
+        "imagen" : "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/7e396cb5-ce48-4822-b170-00aa8877d67c/dfjzmm2-47a1b24f-4f47-4d7c-8d68-42032fb53132.jpg/v1/fill/w_1280,h_720,q_75,strp/tekken_8_wallpaper_with_my_own_tekken_8_logo_by_kaitenkzgraphix_dfjzmm2-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6IlwvZlwvN2UzOTZjYjUtY2U0OC00ODIyLWIxNzAtMDBhYTg4NzdkNjdjXC9kZmp6bW0yLTQ3YTFiMjRmLTRmNDctNGQ3Yy04ZDY4LTQyMDMyZmI1MzEzMi5qcGciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19._eg95mw5R2zc1cgMTCtzyLx4YCBj0KCH1OOLkAf1MAc"
     },
     {
         "id": 4,
@@ -32,7 +34,8 @@ const juegos = [
         "categoria" : "Accion",
         "plataformas" : "Steam",
         "precio" : 15000,
-        "stock" : "Solo clave digital"
+        "stock" : "Solo clave digital",
+        "imagen" : "https://wallpapercave.com/wp/wp3545705.jpg"
         
     },
     {
@@ -41,7 +44,8 @@ const juegos = [
         "categoria" : "Accion",
         "plataformas" : "Steam",
         "precio" : 20000,
-        "stock" : "Solo clave digital"
+        "stock" : "Solo clave digital",
+        "imagen" : "https://i.pinimg.com/originals/b3/41/0a/b3410a93baab7154a78187222b753f27.jpg"
         
     }
 ];
