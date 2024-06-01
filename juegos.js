c063042534d94a79647ee315e58a35c2badfda31
Roberto <kaiser.dark4@outlook.cl>
2024-06-01 17:45:03 -0400

    descripcion

--- juegos.js
+++ juegos.js
@@ -5,6 +5,7 @@ const juegos = [
         "nombre": "Helldivers 2",
         "categoria" : "Accion",
         "plataformas" : "Steam",
+        "descripcion" : "La última línea ofensiva de la galaxia. Enlístate en los Helldivers y únete a la lucha por la libertad en una galaxia hostil en este juego de disparos en tercera persona frenético y feroz.",
         "precio" : 40000,
         "stock" : "Solo clave digital",
         "imagen" : "https://blog.latam.playstation.com/tachyon/sites/3/2023/05/f1d84d4b4ab8f17d61f663da47d92f508897ceab.jpg?resize=1088%2C612&crop_strategy=smart&zoom=1.5"
@@ -14,6 +15,7 @@ const juegos = [
         "nombre": "Dragon Dogma 2",
         "categoria" : "RPG",
         "plataformas" : "Steam",
+        "descripcion" : "Dragons Dogma es un RPG de acción centrado en la narrativa, para un jugador, que desafía a los usuarios a elegir su propia aventura: desde la apariencia de su Ascendido o Ascendida, su clase, sus aliados, cómo enfrentar distintas situaciones y más. Ahora, en esta esperada secuela, el profundo y explorable mundo de fantasía de Dragon's Dogma 2 te espera. ",
         "precio" : 60000,
         "stock" : "Solo clave digital",
         "imagen" : "https://image.api.playstation.com/vulcan/ap/rnd/202311/1309/746f848d0be7107e301df1142745b334b64fbc87c218e3b1.jpg"
@@ -24,6 +26,7 @@ const juegos = [
         "nombre": "Tekken 8",
         "categoria" : "Accion",
         "plataformas" : "Steam",
+        "descripcion" : "El puño se encuentra con el destino en TEKKEN 8. La saga TEKKEN, que ostenta el récord de la trama con mayor duración de un videojuego, se sumerge en un nuevo capítulo con TEKKEN 8. Seis meses después de la conclusión del último enfrentamiento, continúa la trágica historia de los linajes Mishima y Kazama, y los cruentos y amargos combates entre padre e hijo. La historia de crecimiento personal y determinación de Jin Kazama marca un nuevo capítulo en la longeva saga.",
         "precio" : 48000,
         "stock" : "Solo clave digital",
         "imagen" : "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/7e396cb5-ce48-4822-b170-00aa8877d67c/dfjzmm2-47a1b24f-4f47-4d7c-8d68-42032fb53132.jpg/v1/fill/w_1280,h_720,q_75,strp/tekken_8_wallpaper_with_my_own_tekken_8_logo_by_kaitenkzgraphix_dfjzmm2-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6IlwvZlwvN2UzOTZjYjUtY2U0OC00ODIyLWIxNzAtMDBhYTg4NzdkNjdjXC9kZmp6bW0yLTQ3YTFiMjRmLTRmNDctNGQ3Yy04ZDY4LTQyMDMyZmI1MzEzMi5qcGciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19._eg95mw5R2zc1cgMTCtzyLx4YCBj0KCH1OOLkAf1MAc"
@@ -33,6 +36,7 @@ const juegos = [
         "nombre": "Yakuza 0",
         "categoria" : "Accion",
         "plataformas" : "Steam",
+        "descripcion" : "Lucha como el infierno en Tokio y Osaka con el protagonista Kazuma Kiryu y el habitual de la serie Goro Majima. Juega como Kazuma Kiryu y descubre cómo se encuentra en un mundo de problemas cuando un simple cobro de deudas sale mal y su objetivo termina asesinado. Luego, ponte en los zapatos de punta plateada de Goro Majima y explora su vida 'normal' como propietario de un club de cabaret.",
         "precio" : 15000,
         "stock" : "Solo clave digital",
         "imagen" : "https://wallpapercave.com/wp/wp3545705.jpg"
@@ -43,6 +47,7 @@ const juegos = [
         "nombre": "Grand Theft Auto V",
         "categoria" : "Accion",
         "plataformas" : "Steam",
+        "descripcion" : "Grand Theft Auto V para PC ofrece a los jugadores la opción de explorar el galardonado mundo de Los Santos y el condado de Blaine con una resolución de 4K y disfrutar del juego a 60 fotogramas por segundo.",
         "precio" : 20000,
         "stock" : "Solo clave digital",
         "imagen" : "https://i.pinimg.com/originals/b3/41/0a/b3410a93baab7154a78187222b753f27.jpg"
@@ -53,6 +58,7 @@ const juegos = [
         "nombre": "Bloodborne",
         "categoria" : "Accion",
         "plataformas" : "Playstation",
+        "descripcion" : "Enfrenta tus pesadillas mientras buscas respuestas en la antigua ciudad de Yharnam, ahora maldecida por una enfermedad endémica que se propaga por las calles como un fuego arrasador. El peligro, la muerte y la locura merodean en cada esquina de este mundo oscuro y espantoso, y debes descubrir sus secretos más oscuros para poder sobrevivir.",
         "precio" : 20000,
         "stock" : "Juego disponible",
         "imagen" : "https://wallpapercave.com/wp/wp7546334.jpg"
